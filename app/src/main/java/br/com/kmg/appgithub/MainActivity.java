4a45e155c56e234d013819451d4695dfd08e8a82
Klinsman Maia <klinsman.goncalves@gmail.com>
2020-05-22 01:23:20 -0400

    Initial commit

---
+++ app/src/main/java/br/com/kmg/appgithub/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.kmg.appgithub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
