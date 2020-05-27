d957d0ae33e1979fe6c745279780077cc6872224
Klinsman Maia <klinsman.goncalves@gmail.com>
2020-05-27 01:57:09 -0400

    build URL

--- app/src/main/java/br/com/kmg/appgithub/MainActivity.java
+++ app/src/main/java/br/com/kmg/appgithub/MainActivity.java
@@ -3,12 +3,55 @@ package br.com.kmg.appgithub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URL;
+
+import br.com.kmg.appgithub.util.NetworkUtil;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    EditText edQuery;
+    Button btSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edQuery = findViewById(R.id.edQuery);
+        btSearch = findViewById(R.id.btSearch);
+        setListeners();
+
     }
+
+    public void setListeners(){
+        btSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchRepositories(edQuery.getText().toString());
+            }
+        });
+
+    }
+     public void searchRepositories(String query){
+         URL url = NetworkUtil.getURL(query);
+         Log.d(TAG, "URL: " + url);
+         Toast.makeText(getApplicationContext(), "Acessando URL", Toast.LENGTH_LONG).show();
+
+         try {
+             String json = NetworkUtil.getResponseFromHttpUrl(url);
+             Log.d(TAG, json);
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+
+     }
+
 }
